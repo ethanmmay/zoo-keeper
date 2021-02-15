de88fde413da3f438fe07b52708d786452ece313
ethanmmay <ethaniel.may@gmail.com>
2021-02-15 11:41:04 -0700

    Added Monkeys

--- app/main.js
+++ app/main.js
@@ -1,3 +1,13 @@
 import Alligator from "./models/Alligator.js";
+import Monkey from "./models/Monkey.js"
 
-let ally = new Alligator("Ally", "Green", "F", 200, 60, "big");
+let ally = new Alligator("Ally", "Green", "F", 200, 60, "big")
+let avery = new Alligator("Avery", "Dark Green", "F", 220, 65, "large")
+let marty = new Monkey("Marty", "Brown", "M", 160)
+let mandy = new Monkey("Mandy", "Gold", "F", 150)
+
+let monkeys = [marty, mandy]
+let alligators = [ally, avery]
+
+monkeys[0].eat("banana")
+alligators[1].eat("chicken")
--- app/models/Monkey.js
+++ app/models/Monkey.js
@@ -1,16 +1,14 @@
-class Alligator {
-    constructor(name, color, gender, weight, tailLengthInch, teethSize) {
+class Monkey {
+    constructor(name, color, gender, weight) {
         this.id = 0,
         this.name = name || ''
         this.color = color || ''
         this.gender = gender || ''
         this.weight = weight || 0
-        this.tailLengthInch = tailLengthInch || 0
-        this.teethSize = teethSize || ''
     }
     eat(food){
         console.log(`${this.name} is chomp chomping all the ${food}ies.`)
     }
 }
 
-export default Alligator;
+export default Monkey;
