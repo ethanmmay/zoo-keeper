3431caf027c7e2623e62440ca933e9734e741e94
ethanmmay <ethaniel.may@gmail.com>
2021-02-15 12:02:20 -0700

    Monkeys & Alligators

--- app/main.js
+++ app/main.js
@@ -1,13 +1,13 @@
 import Alligator from "./models/Alligator.js";
 import Monkey from "./models/Monkey.js"
 
-let ally = new Alligator("Ally", "Green", "F", 200, 60, "big")
-let avery = new Alligator("Avery", "Dark Green", "F", 220, 65, "large")
-let marty = new Monkey("Marty", "Brown", "M", 160)
-let mandy = new Monkey("Mandy", "Gold", "F", 150)
+let monkeys = []
+let alligators = []
 
-let monkeys = [marty, mandy]
-let alligators = [ally, avery]
+alligators.push(new Alligator("Ally", "Green", "F", 200, 60, "big"))
+alligators.push(new Alligator("Avery", "Dark Green", "F", 220, 65, "large"))
+monkeys.push(new Monkey("Marty", "Brown", "M", 160))
+monkeys.push(new Monkey("Mandy", "Gold", "F", 150))
 
-monkeys[0].eat("banana")
-alligators[1].eat("chicken")
+monkeys.forEach(m => m.eat("banana"))
+alligators.forEach(a => a.eat("chicken"))
